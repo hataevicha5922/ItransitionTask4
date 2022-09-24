0b378f7e561edee3abd47a3b156a69b744bec6db
hataevicha5922 <hataevicha5922@gmail.com>
2022-09-25 01:04:44 +0300

    add table

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import TableComponent from '../../components/table/Table';
+import ToolBar from '../../components/toolBar/ToolBar';
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <Container>
+      <ToolBar />
+      <TableComponent />
+    </Container>
+  );
 };
 
 export default Main;
